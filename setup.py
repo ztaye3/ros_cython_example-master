68ae92248823767ddf72440e7243c048b48e3c0b
ztaye <ztaye3@gmail.com>
2021-09-16 21:18:03 +0200

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['cython_catkin_example'],
+    packages=['ros_cython_example'],
     package_dir={'': 'src'},
     requires=['std_msgs', 'rospy']
 )
